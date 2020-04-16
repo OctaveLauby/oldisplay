d2416ff0171925dd359c1ce8b8c129c6b4722407
OctaveLauby <octavelauby@gmail.com>
2020-04-16 16:51:01 +0200

    Indentation

--- oldisplay/components/shape.py
+++ oldisplay/components/shape.py
@@ -6,78 +6,82 @@ from .component import Component
 
 
 class SurfaceShape(Component):
+    """Base class for 2d shapes
 
-        dft_look = {
-            'color': "white",
-            'outline': "black",
-            'width': None,
-        }
+    Handle an outline and look changes when hovered or clicked
+    """
 
-        @classmethod
-        @abstractmethod
-        def build_cache(cls, *args, **kwargs):
-            raise NotImplementedError
+    dft_look = {
+        'color': "white",
+        'outline': "black",
+        'width': None,
+    }
 
-        def __init__(self, args=None, kwargs=None, look=None):
-            """Initiate instance of 2d-shape
+    @classmethod
+    @abstractmethod
+    def build_cache(cls, *args, **kwargs):
+        raise NotImplementedError
 
-            Args:
-                args (tuple)    : args for shape building
-                kwargs (dict)   : kwargs for shape building
-                look (dict)     : aspect description
-                    color (color description)   : inside color
-                    outline (color description) : outline color
-                    width (int)                 : width of outline
-                    hovered (dict)              : aspect when mouse over shape
-                    clicked (dict)              : aspect when click on shape
-            """
-            super().__init__()
-            args = {} if args is None else args
-            kwargs = {} if kwargs is None else kwargs
-            look = {} if look is None else look
+    def __init__(self, args=None, kwargs=None, look=None):
+        """Initiate instance of 2d-shape
 
-            # Read Looks
-            hovered = look.pop('hovered', None)
-            clicked = look.pop('clicked', None)
-            self._look = read_params(look, self.cls.dft_look)
-            self._look_h = (
-                None if hovered is None
-                else read_params(hovered, self._look)
-            )
-            self._look_c = (
-                None if clicked is None
-                else read_params(clicked, self._look_h or self._look)
-            )
-            for look in [self._look, self._look_h, self._look_c]:
-                if look is None:
-                    continue
-                for key in ['color', 'outline']:
-                    look[key] = Color.get(look[key])
+        Args:
+            args (tuple)    : args for shape building
+            kwargs (dict)   : kwargs for shape building
+            look (dict)     : aspect description
+                color (color description)   : inside color
+                outline (color description) : outline color
+                width (int)                 : width of outline
+                hovered (dict)              : aspect when mouse over shape
+                clicked (dict)              : aspect when click on shape
+        """
+        super().__init__()
+        args = tuple() if args is None else args
+        kwargs = {} if kwargs is None else kwargs
+        look = {} if look is None else look
 
-            # Build elements to draw
-            self._cache = self.cls.build_cache(*args, **kwargs)
+        # Read Looks
+        hovered = look.pop('hovered', None)
+        clicked = look.pop('clicked', None)
+        self._look = read_params(look, self.cls.dft_look)
+        self._look_h = (
+            None if hovered is None
+            else read_params(hovered, self._look)
+        )
+        self._look_c = (
+            None if clicked is None
+            else read_params(clicked, self._look_h or self._look)
+        )
+        for look in [self._look, self._look_h, self._look_c]:
+            if look is None:
+                continue
+            for key in ['color', 'outline']:
+                look[key] = Color.get(look[key])
 
-        @property
-        def cache(self):
-            """Return surface of shape"""
-            return self._cache
+        # Build elements to draw
+        self._cache = self.cls.build_cache(*args, **kwargs)
 
-        @abstractmethod
-        def display_(self, surface, color, outline, width):
-            raise NotImplementedError
+    @property
+    def cache(self):
+        """Return surface of shape"""
+        return self._cache
 
-        def display_normal(self, surface, *args, **kwargs):
-            """Display shape with normal aspect"""
-            return self.display_(surface, **self._look)
+    @abstractmethod
+    def display_(self, surface, color, outline, width):
+        raise NotImplementedError
 
-        def display_hovered(self, surface, *args, **kwargs):
-            """Display shape with hovered aspect"""
-            if self._look_h is None:
-                return self.display_normal(surface, *args, **kwargs)
-            return self.display_(surface, **self._look_h)
+    def display_normal(self, surface, *args, **kwargs):
+        """Display shape with normal aspect"""
+        return self.display_(surface, **self._look)
 
-        def display_clicked(self, surface, *args, **kwargs):
-            """Display shape with clicked aspect"""
-            if self._look_c is None:
-                return self.display_hovered(surface, *args, **kwargs)
-            return self.display_(surface, **self._look_c)
+    def display_hovered(self, surface, *args, **kwargs):
+        """Display shape with hovered aspect"""
+        if self._look_h is None:
+            return self.display_normal(surface, *args, **kwargs)
+        return self.display_(surface, **self._look_h)
+
+    def display_clicked(self, surface, *args, **kwargs):
+        """Display shape with clicked aspect"""
+        if self._look_c is None:
+            return self.display_hovered(surface, *args, **kwargs)
+        return self.display_(surface, **self._look_c)
