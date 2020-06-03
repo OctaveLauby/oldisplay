d83d0eb330f5de0faa76d8a13c584979e73c627e
OctaveLauby <octavelauby@gmail.com>
2020-06-03 19:29:08 +0200

    Add inner grid option

--- oldisplay/components/grid.py
+++ oldisplay/components/grid.py
@@ -6,14 +6,24 @@ from .line import LineSet
 class Grid(LineSet):
     """Grid that fills a surface"""
 
-    def __init__(self, dx, dy, xbounds=None, ybounds=None, **kwargs):
-        """Initiate a grid"""
+    def __init__(self, dx, dy, xbounds=None, ybounds=None,
+                 inner_grid=False, **kwargs):
+        """Initiate a grid
+
+        Args:
+            dx (int): nb of pixels b/w 2 vertical lines
+            dy (int): nb of pixels b/w 2 horizontal lines
+            xbounds (2-int-tuple)   : x-boundaries of grid
+            ybounds (2-int-tuple)   : y-boundaries of grid
+            inner_grid (bool)       : draw only inner lines
+        """
         assert dx > 0, dy > 0
         super().__init__(lines=[], **kwargs)
         self.dx = dx
         self.dy = dy
         self.xbounds = (0, np.inf) if xbounds is None else xbounds
         self.ybounds = (0, np.inf) if ybounds is None else ybounds
+        self.inner_grid = inner_grid
 
     def display(self, surface, **params):
         """Display grid on surface"""
@@ -28,13 +38,19 @@ class Grid(LineSet):
         # Vertical lines
         x = x_min
         while x <= x_max:
-            lines.append([(x, y_min), (x, y_max)])
+            if self.inner_grid and x in [x_min, x_max]:
+                pass
+            else:
+                lines.append([(x, y_min), (x, y_max)])
             x += self.dx
 
         # Horizontal lines
         y = y_min
         while y <= y_max:
-            lines.append([(x_min, y), (x_max, y)])
+            if self.inner_grid and y in [y_min, y_max]:
+                pass
+            else:
+                lines.append([(x_min, y), (x_max, y)])
             y += self.dy
 
         # -- Display
