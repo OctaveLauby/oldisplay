5112122649675b2e2bfa2f7d6993f4bb475cfc38
OctaveLauby <octavelauby@gmail.com>
2020-06-03 19:22:04 +0200

    add bounds param to grid

--- oldisplay/components/grid.py
+++ oldisplay/components/grid.py
@@ -1,16 +1,19 @@
 """Objects to draw grids"""
+import numpy as np
 from .line import LineSet
 
 
 class Grid(LineSet):
     """Grid that fills a surface"""
 
-    def __init__(self, dx, dy, **kwargs):
+    def __init__(self, dx, dy, xbounds=None, ybounds=None, **kwargs):
         """Initiate a grid"""
         assert dx > 0, dy > 0
         super().__init__(lines=[], **kwargs)
         self.dx = dx
         self.dy = dy
+        self.xbounds = (0, np.inf) if xbounds is None else xbounds
+        self.ybounds = (0, np.inf) if ybounds is None else ybounds
 
     def display(self, surface, **params):
         """Display grid on surface"""
@@ -18,17 +21,20 @@ class Grid(LineSet):
         # -- Build lines
         lines = []
         sx, sy = surface.get_size()
+        x_min, x_max = self.xbounds
+        y_min, y_max = self.ybounds
+        x_max, y_max = min(x_max, sx), min(y_max, sy)
 
         # Vertical lines
-        x = 0
-        while x <= sx:
-            lines.append([(x, 0), (x, sy)])
+        x = x_min
+        while x <= x_max:
+            lines.append([(x, y_min), (x, y_max)])
             x += self.dx
 
         # Horizontal lines
-        y = 0
-        while y <= sy:
-            lines.append([(0, y), (sx, y)])
+        y = y_min
+        while y <= y_max:
+            lines.append([(x_min, y), (x_max, y)])
             y += self.dy
 
         # -- Display
